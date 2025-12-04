5630fa591d9adb2a7c870f61cf0bff84503ef1e5
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2025-12-04 11:15:26 +0100

    test: update thresholds

--- jest.config.js
+++ jest.config.js
@@ -34,10 +34,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 5,
+      functions: 5,
+      lines: 5,
+      statements: 5,
     },
   },
   moduleNameMapper: {
